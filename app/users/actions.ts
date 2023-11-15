e68fa4b132614ee9705743ab1d748bff1f5641c7
orzorzooo <orzorzooo@gmail.com>
2023-11-15 10:33:59 +0800

    EDIT: users/action加入 server 倒頁機制

--- app/users/actions.ts
+++ app/users/actions.ts
@@ -1,11 +1,11 @@
 "use server";
-// import { db, users } from "@/lib/db/users";
 import { db, users } from "@/lib/drizzle";
 import type { Users, NewUser } from "@/lib/drizzle";
+import { revalidatePath } from "next/cache";
 
 export const createUsers = async (user: NewUser) => {
   user.password = "123";
-  console.log(user);
   const result = await db.insert(users).values(user);
+  revalidatePath("/users");
   console.log("result", result);
 };
