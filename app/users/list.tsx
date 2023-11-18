f02a27dc3e81ec6c8a8693e08a460f80064fe8d7
orzorzooo <orzorzooo@gmail.com>
2023-11-18 10:51:53 +0800

    ADD: animejs 效果

--- app/users/list.tsx
+++ app/users/list.tsx
@@ -2,7 +2,7 @@
 import type { Users } from "../../lib/drizzle";
 import { deleteUser } from "./actions";
 import { Button } from "../../components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Dialog,
   DialogContent,
@@ -17,6 +17,8 @@ import {
 import { ReloadIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 
+import anime from "animejs";
+
 export function List({ allUsers }: any) {
   const [pending, setPending] = useState(false);
   async function onDelete(user: Users) {
@@ -25,12 +27,20 @@ export function List({ allUsers }: any) {
     setPending(false);
   }
   if (!allUsers) return false;
-
+  useEffect(() => {
+    const animation = anime({
+      targets: ".orz-user-li",
+      translateX: -10,
+      delay: anime.stagger(100),
+      opacity: 1,
+    });
+    animation.play();
+  });
   return (
     <ul className="w-1/2">
       {allUsers.map((user: Users) => (
         <Link href={`users/${user.id}`} key={user.id}>
-          <li className="flex justify-between items-center p-5 border  border-gray-200 my-3 rounded-xl">
+          <li className="flex justify-between items-center p-5 border  border-gray-200 my-3 rounded-xl -translate-y-8 opacity-0 orz-user-li">
             <div>{user.id}</div>
 
             <div className="font-bold text-xl">{user.name}</div>
